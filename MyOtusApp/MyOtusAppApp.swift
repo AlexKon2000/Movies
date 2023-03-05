12d88f97ad2c425702fc1bbd1ad2b0de9933489c
Alex <myname83@list.ru>
2023-03-05 23:43:00 +0300

    Initial Commit

---
+++ MyOtusApp/MyOtusAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyOtusAppApp.swift
+//  MyOtusApp
+//
+//  Created by Alex on 05.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct MyOtusAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
